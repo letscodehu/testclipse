9e732e66fc701d79c6ef8112bd2c085f44fa1c80
Papp Krisztián <fejlesztes@letscode.hu>
2016-09-15 00:55:30 +0200

    Initial commit

---
+++ templates/junit-mockitorunner.java
@@ -0,0 +1,21 @@
+package {packageName};
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.rucsok.rucsok.service.helper.GeneralDocumentParser;
+
+@RunWith(MockitoJUnitRunner.class)
+public class {className} {
+
+}
